94b49176a3fcd27ea7fb1fd48cc4daa87707a82d
parra <parrapartycar@gmail.com>
2015-01-25 20:09:07 +0100

    Compute hash sum on specified file extension to verify data integrity.

--- archiver.py
+++ archiver.py
@@ -31,7 +31,7 @@ class Archiver:
 
 
 	def usage(self):
-		print("usage")
+		print("./archive.py")
 
 
 	def initProcessInfo(self, namespace):
@@ -88,7 +88,7 @@ class Archiver:
 			sys.exit(3)
 
 
-	def buildCommands(self, namespace, name):
+	def buildCommands(self, namespace):
 		''' Replaces command templates and builds a list of commands
 			ready to be executed.
 			Returns a list of commands
@@ -97,15 +97,21 @@ class Archiver:
 		self.initProcessInfo(namespace)
 
 		for source in self.sources:
-			print("{} {}".format(name, source['host']))
+			print("{} {} ...".format(namespace.capitalize(), source['host']))
 			builtFolder = self.buildTargetFolder(source['host'], source['folder'])
 			# fill command template with the right parameters
-			if name == 'archive':
+			if namespace == 'archive':
 				command = 'rsync -avz {}@{}:{}/ {}'
 				cmd = command.format(source['user'], source['host'], source['folder'], builtFolder)
-			elif name == 'clean':
+			elif namespace == 'clean':
 				command = 'ssh {}@{} find {} -type f -name \"*.{}\" -mtime +{} -exec rm {{}} \;'
 				cmd = command.format(source['user'], source['host'], source['folder'], source['extension'], source['maxAge'])
+			elif namespace == 'localchecksum':
+				command = "find {} -type f -name *.{} -exec md5sum {{}} ;"
+				cmd = command.format(builtFolder, source['extension'])
+			elif namespace == 'remotechecksum':
+				command = 'ssh {}@{} find {} -type f -name \"*.{}\" -exec md5sum {{}} \;'
+				cmd = command.format(source['user'], source['host'], source['folder'], source['extension'])
 			else:
 				print("Invalid command")
 				sys.exit(4)
@@ -121,7 +127,7 @@ class Archiver:
 		''' Copies all files from sources to a target folder, one source at a time
 		'''
 		name = namespace = self.archive.__name__
-		procs = self.buildCommands(namespace, name)
+		procs = self.buildCommands(namespace)
 		self.runProcs(namespace, procs)
 
 
@@ -129,7 +135,7 @@ class Archiver:
 		''' Write stdout and stderr to log files
 		'''
 		# Format: 'date - host - line separator - output'
-		logFormat = '{} - {} - {}'
+		logFormat = '{} - {} - {}\n'
 		if settings.logging:
 			for server, result in self.processInfo[namespace].items():
 				if len(self.processInfo[namespace][server]['stdout']) > 0:
@@ -138,19 +144,71 @@ class Archiver:
 					self.logFileError.write(logFormat.format(datetime.now(), server, self.processInfo[namespace][server]['stderr']))
 
 
+	def verifyChecksums(self):
+		''' Read source and destination hash sums to determine
+			if transfer was successfully finished.
+                        Important: checksum is calculated only for specified file extension.
+		'''
+		if self.errors:
+			print("Won't execute *checksum* verification because of previous errors. Exiting.")
+			sys.exit(6)
+
+                # target folder (copied files)
+		name = namespace = localNamespace = 'localchecksum'
+		procs = self.buildCommands(namespace)
+		self.runProcs(namespace, procs)
+
+                # source folder (original files)
+		name = namespace = remoteNamespace = 'remotechecksum'
+		procs = self.buildCommands(namespace)
+		self.runProcs(namespace, procs)
+
+		for source in self.sources:
+			if not self.compareHashes(self.processInfo[localNamespace][source['host']]['stdout'], self.processInfo[remoteNamespace][source['host']]['stdout']):
+				print("Checksum error on host {}".format(source['host']))
+				sys.exit(5)
+
+
+	def getHashes(self, stdout):
+		''' Returns a list of hashes extracted from a process output
+			formated as 'md5sum_hash  filename'
+		'''
+		stdout = stdout.decode()
+		a = stdout.split('\n')
+		result = []
+		for line in a:
+			list = line.split('  ')
+			result.append(list[0])
+		return result
+
+
+	def compareHashes(self, stdoutA, stdoutB):
+		''' Compares two process outputs and returns a boolean
+		'''
+		if len(stdoutA) == 0 or len(stdoutB) == 0:
+			return false
+
+		hashesA = self.getHashes(stdoutA)
+		hashesB = self.getHashes(stdoutB)
+		return hashesA == hashesB
+
+
 	def clean(self):
 		''' Clean all copied files from sources, one source at a time
 		'''
-		raise NotImplementedError("WIP. Must implement checksum checking before.")
+		if self.errors:
+			print("Won't execute *clean* because of previous errors. Exiting.")
+			sys.exit(7)
 
 		name = namespace = self.clean.__name__
-		procs = self.buildCommands(namespace, name)
+		procs = self.buildCommands(namespace)
 		self.runProcs(namespace, procs)
 
 
 	def run(self):
 		archiver.archive()
-		archiver.clean()
+		archiver.verifyChecksums()
+		#archiver.clean()
 
 
 
