25ac4dbd60f58add472e88bc8c2bce96d2434128
Roger Sicart <roger@adserverpub.com>
2015-01-26 13:02:09 +0100

    Avoid duplication with a new method to launch a list of processes.

--- archiver.py
+++ archiver.py
@@ -62,24 +62,12 @@ class Archiver:
 		return folder
 
 
-	def archive(self):
-		''' Copies all files from sources to a target folder, one source at a time
+	def runProcs(self, namespace, processList):
+		''' Checks a list of processes and removes the processes from the list when ready.
+			Tries to get stdout, stderr and return code.
 		'''
-		# command template
-		command = 'rsync -avz {}@{}:{}/ {}'
-		namespace = 'archive'
-		procs = []
-		self.initProcessInfo(namespace)
-		for source in self.sources:
-			print("Archiving {}".format(source['host']))
-			builtFolder = self.buildTargetFolder(source['host'], source['folder'])
-			cmd = command.format(source['user'], source['host'], source['folder'], builtFolder)
-			proc = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-			procs.append({'host': source['host'], 'proc': proc})
-			print(cmd)
-
-		while procs:
-			for p in procs:
+		while processList:
+			for p in processList:
 				server, proc = p['host'], p['proc']
 				if proc.poll() is not None:
 					self.processInfo[namespace][server]['stdout'] = proc.stdout.read()
@@ -91,17 +79,57 @@ class Archiver:
 						self.errors = True
 						print(self.processInfo[namespace][server]['stderr'])
 					# Delete finished process
-					procs.remove(p)
+					processList.remove(p)
 
 		self.logProcessInfo(namespace)
 
 		if self.errors:
+			print('Method with namespace {} produced some errors. For detailed info read log files.'.format(namespace))
 			sys.exit(3)
 
 
+	def buildCommands(self, namespace, name):
+		''' Replaces command templates and builds a list of commands
+			ready to be executed.
+			Returns a list of commands
+		'''
+		procs = []
+		self.initProcessInfo(namespace)
+
+		for source in self.sources:
+			print("{} {}".format(name, source['host']))
+			builtFolder = self.buildTargetFolder(source['host'], source['folder'])
+			# fill command template with the right parameters
+			if name == 'archive':
+				command = 'rsync -avz {}@{}:{}/ {}'
+				cmd = command.format(source['user'], source['host'], source['folder'], builtFolder)
+			elif name == 'clean':
+				command = 'ssh {}@{} find {} -type f -name \"*.{}\" -mtime +{} -exec rm {{}} \;'
+				cmd = command.format(source['user'], source['host'], source['folder'], source['extension'], source['maxAge'])
+			else:
+				print("Invalid command")
+				sys.exit(4)
+
+			proc = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+			procs.append({'host': source['host'], 'proc': proc})
+			print(cmd)
+
+		return procs
+
+
+	def archive(self):
+		''' Copies all files from sources to a target folder, one source at a time
+		'''
+		name = namespace = self.archive.__name__
+		procs = self.buildCommands(namespace, name)
+		self.runProcs(namespace, procs)
+
+
 	def logProcessInfo(self, namespace):
+		''' Write stdout and stderr to log files
+		'''
 		# Format: 'date - host - line separator - output'
-		logFormat = '{} - {}\n==============================\n{}'
+		logFormat = '{} - {} - {}'
 		if settings.logging:
 			for server, result in self.processInfo[namespace].items():
 				if len(self.processInfo[namespace][server]['stdout']) > 0:
@@ -113,42 +141,19 @@ class Archiver:
 	def clean(self):
 		''' Clean all copied files from sources, one source at a time
 		'''
-		# command template
-		command = 'ssh {}@{} find {} -type f -name \"*.{}\" -mmin +{} -exec rm {{}} \;'
-		#command = 'ssh {}@{} find {} -type f -name \"*.{}\" -mtime +{} -exec rm {{}} \;'
-		namespace = 'clean'
-		self.initProcessInfo(namespace)
-		procs = []
-		for source in self.sources:
-			print("Cleaning {}".format(source['host']))
-			cmd = command.format(source['user'], source['host'], source['folder'], source['extension'], source['maxAge'])
-			print(cmd)
-			proc = subprocess.Popen(cmd.split(), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-			procs.append({'host': source['host'], 'proc': proc})
+		raise NotImplementedError("WIP. Must implement checksum checking before.")
 
-		while procs:
-			for p in procs:
-				server, proc = p['host'], p['proc']
-				if proc.poll() is not None:
-					self.processInfo[namespace][server]['stdout'] = proc.stdout.read()
-					self.processInfo[namespace][server]['stderr'] = proc.stderr.read()
-					self.processInfo[namespace][server]['returncode'] = proc.returncode
-					if self.verbose:
-						print(self.processInfo[namespace][server]['stdout'])
-					if proc.returncode > 0 or len(self.processInfo[namespace][server]['stderr']) > 0:
-						self.errors = True
-						print(self.processInfo[namespace][server]['stderr'])
-					# Delete finished process
-					procs.remove(p)
+		name = namespace = self.clean.__name__
+		procs = self.buildCommands(namespace, name)
+		self.runProcs(namespace, procs)
 
-		self.logProcessInfo(namespace)
 
-		if self.errors:
-			sys.exit(3)
+	def run(self):
+		archiver.archive()
+		archiver.clean()
 
 
 
 if __name__ == '__main__':
 	archiver = Archiver()
-	archiver.archive()
-	archiver.clean()
+	archiver.run()
